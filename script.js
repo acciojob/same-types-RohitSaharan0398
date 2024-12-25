76ec7c5c40802fe63fba31512384a26de43a1c2d
RohitSaharan0398 <rohit8607745768@gmail.com>
2024-12-25 12:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function isSameType(value1, value2) {
   //your js code here
-	if(typeof(value1)==typeof(value2))
-		return true;
-	else return false;
+	 if (isNaN(value1) && isNaN(value2)) {
+    return true;
+  } else if (typeof(value1) === typeof(value2)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // do not change the code below.
